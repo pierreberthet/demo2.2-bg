3628a3a25bb1225a5bf49d4f3895c56abc9a36fc
pierreberthet <berthetp@gmail.com>
2014-12-10 15:18:08 +0100

    - added draft of the inputs population and poisson generators - added skeleton of the weight update function

--- pynn/demo.2.2.py
+++ pynn/demo.2.2.py
@@ -20,6 +20,11 @@ Vreset = -75.
 Vth = -50.
 gL = 16.666
 
+inactive_state_rate = 1000.
+active_state_rate = 1600.
+
+
+
 
 #neurons models and parameters
 
@@ -32,6 +37,18 @@ neuron = sim.IF_cond_alpha(params)
 #  POPULATIONS
 # #############
 #CORTEX input population: N states, poisson inputs
+
+inputs = {}
+for i in xrange(n_states):
+    inputs[i] = sim.Population(n_cortex_cells, SpikeSourcePoisson, {'rate': inactive_state_rate}, "poisson_iput_"+str(i))
+
+
+def set_state(i_state):
+    for i in xrange(n_states):
+	sim.SetStatus(inputs[i], {'rate':inactive_state_rate})
+    sim.SetStatus(inputs[i_state], {'rate':active_State_rate})
+    
+
 
 #?assemblies of n_actions populations or dictionnary of populations?
 #STRIATUM 2 populations of M actions, D1 and D2
@@ -81,6 +98,31 @@ neuron = sim.IF_cond_alpha(params)
 #	offline computations of selection, reward, and update
 #	load weights
 #	new trial
+
+
+def update_weights(i_state,  spike_trains):
+    #Update the weights based on the state and the output spike trains
+    #TODO  reward function that computes the reward given the curernt state and the selected action
+    #TODO  store weights value for all the trials
+    #TODO  decide how to set kappa and how to artificially increase the traces of the selected action
+    #TODO  update function
+
+    zi_ += (spike_height * yi_ - zi_ + epsilon_ ) * resolution / taui_;
+    zj_ += (K_ < 0) ? (1./1000.0 - yj_/fmax_ - zj_ + epsilon_)
+                * resolution / tauj_  : (spike_height * yj_ - zj_ + epsilon_ ) * resolution / tauj_;
+
+
+    /* Secondary synaptic traces */
+    ei_  += (zi_ - ei_) * resolution / taue_;
+    ej_  += (zj_ - ej_) * resolution / taue_;
+    eij_ += (zi_ * zj_ - eij_) * resolution / taue_;
+
+    /* Tertiary synaptic traces. Commented is from Wahlgren paper. */
+    pi_  +=std::abs( K_) * (ei_ - pi_) * resolution / taup_/* * eij_*/;
+    pj_  +=std::abs( K_) * (ej_ - pj_) * resolution / taup_/* * eij_*/;
+    pij_ +=std::abs( K_ )* (eij_ - pij_) * resolution / taup_/* * eij_*/;
+
+
 
 
 
